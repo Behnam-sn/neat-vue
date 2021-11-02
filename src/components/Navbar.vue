6af94d5f4aa8342bd1742fcc0388478b69e14b73
behnam <salehinezhad.b@gmail.com>
2021-11-02 17:53:24 +0330

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+	<div
+		id="nav"
+		class="flex flex-row justify-between items-center px-6 py-7 lg:hidden"
+	>
+		<div>Neat</div>
+		<div class="flex flex-row">
+			<MoonIcon class="mr-5" />
+			<BarsIcon />
+		</div>
+		<!-- <div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</div> -->
+	</div>
+</template>
+
+<script>
+import BarsIcon from "@/components/svg/BarsIcon.vue";
+import MoonIcon from "@/components/svg/MoonIcon.vue";
+
+export default {
+	name: "Navbar",
+	components: {
+		BarsIcon,
+		MoonIcon,
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+// #nav {
+// 	padding: 30px;
+
+// 	a {
+// 		font-weight: bold;
+// 		color: #2c3e50;
+
+// 		&.router-link-exact-active {
+// 			color: #42b983;
+// 		}
+// 	}
+// }
+</style>
