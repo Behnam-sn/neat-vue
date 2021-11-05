c8899e1606d4d35c0ca4d4f8b2bc21421420ef72
behnam <salehinezhad.b@gmail.com>
2021-11-06 01:39:52 +0330

    add axios

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './assets/tailwind.css'
+import Vue from "vue";
+import axios from "axios";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/tailwind.css";
+
+Vue.config.productionTip = false;
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/"; // the FastAPI backend
+
+axios.interceptors.response.use(undefined, function(error) {
+	if (error) {
+		const originalRequest = error.config;
+		if (error.response.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+		}
+	}
+});
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -5,27 +5,39 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		darkMode: localStorage.theme,
+		darkMode: localStorage.getItem("theme"),
 		isCollapse: false,
+		user: localStorage.getItem("user"),
+		token: localStorage.getItem("token"),
 	},
 	getters: {
 		getTheme: (state) => state.darkMode,
 		getCollapseStatus: (state) => state.isCollapse,
+		getUser: (state) => state.user,
+		getToken: (state) => state.token,
 	},
 	mutations: {
 		changeTheme(state) {
 			if (localStorage.theme == "light") {
-				localStorage.theme = "dark";
+				localStorage.setItem("theme", "dark");
 				state.darkMode = "dark";
 				document.documentElement.classList.add("dark");
 			} else {
-				localStorage.theme = "light";
+				localStorage.setItem("theme", "light");
 				state.darkMode = "light";
 				document.documentElement.classList.remove("dark");
 			}
 		},
 		collapse(state) {
 			state.isCollapse = !state.isCollapse;
+		},
+		setUser(state, user) {
+			state.user = user;
+			localStorage.setItem("user", user);
+		},
+		setToken(state, token) {
+			state.token = token;
+			localStorage.setItem("token", token);
 		},
 	},
 	actions: {},
