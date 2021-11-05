6b4aec034b87668f58c556e0045a8819bbd8621a
behnam <salehinezhad.b@gmail.com>
2021-11-06 01:40:25 +0330

    add login api

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,18 +3,20 @@
 		<h1 class="hidden lg:block font-Poppins-Bold text-6xl mb-32">Neat</h1>
 		<div class="flex flex-col items-center">
 			<button
-				class="font-Poppins-Light border-2 border-gray-400 rounded-full px-6 py-2 self-end"
 				@click="changeMode"
+				class="font-Poppins-Light border-2 border-gray-400 rounded-full px-6 py-2 self-end"
 			>
 				{{ signup ? "Login" : "Sign Up" }}
 			</button>
 			<input
-				class="font-Poppins-Light bg-gray-200 dark:bg-gray-800 text-lg w-80 px-4 py-2 mt-8 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50 transition duration-500"
+				v-model="username"
+				class="font-Poppins-Light bg-gray-200 dark:bg-gray-800 text-lg w-80 px-4 py-2 mt-8 rounded-lg focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white focus:ring-opacity-50 transition duration-500"
 				type="text"
 				name="username"
 				placeholder="Username"
 			/>
 			<input
+				v-model="fullname"
 				class="font-Poppins-Light bg-gray-200 dark:bg-gray-800 text-lg w-80 px-4 py-2 mt-5 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50 transition duration-500"
 				type="text"
 				name="fullname"
@@ -22,12 +24,14 @@
 				v-if="signup"
 			/>
 			<input
+				v-model="password"
 				class="font-Poppins-Light bg-gray-200 dark:bg-gray-800 text-lg w-80 px-4 py-2 mt-5 rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50 transition duration-500"
 				type="password"
 				name="password"
 				placeholder="Password"
 			/>
 			<button
+				@click="submit"
 				class="font-Poppins-Medium text-xl bg-black text-customWhite dark:bg-customWhite dark:text-black rounded-full px-16 py-3 mt-12 transition duration-500"
 			>
 				{{ signup ? "Sign Up" : "Login" }}
@@ -37,6 +41,9 @@
 </template>
 
 <script>
+import axios from "axios";
+import { mapMutations } from "vuex";
+
 export default {
 	name: "Login",
 	data: () => ({
@@ -46,8 +53,24 @@ export default {
 		password: "",
 	}),
 	methods: {
+		...mapMutations(["setUser", "setToken"]),
 		changeMode() {
 			this.signup = !this.signup;
+		},
+		submit() {
+			const User = new FormData();
+			User.append("username", this.username);
+			User.append("password", this.password);
+
+			axios
+				.post("auth/login", User)
+				.then((response) => {
+					this.setToken(response.data.access_token);
+					this.setUser(this.username);
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
 		},
 	},
 };
