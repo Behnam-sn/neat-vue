c188bcdf4ca935016bf0715ea9d6dabc4646de36
behnam <salehinezhad.b@gmail.com>
2021-11-10 03:51:14 +0330

    capitalize title

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="h-vh-15 flex justify-between items-center px-7 lg:px-16">
-		<h1 class="font-Poppins-Bold text-5xl lg:text-6xl">{{ title }}</h1>
+		<h1 class="font-Poppins-Bold text-5xl lg:text-6xl capitalize">
+			{{ title }}
+		</h1>
 		<LoginButton class="hidden lg:block" v-if="!user" />
 	</div>
 </template>
