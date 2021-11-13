2c04c2608a19d397c099bd62576089b8c5e4726c
behnam <salehinezhad.b@gmail.com>
2021-11-13 15:19:23 +0330

    update Settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,21 @@
-<template>
-	<div>Settings</div>
-</template>
-
 <script>
+import TitleBar from "../components/TitleBar.vue";
+
 export default {
 	name: "Settings",
+	components: {
+		TitleBar,
+	},
 };
 </script>
 
-<style></style>
+<template>
+	<div>
+		<TitleBar title="Settings" />
+		<div>username</div>
+		<div>password</div>
+		<div>full name</div>
+		<button>delete</button>
+		<button>done</button>
+	</div>
+</template>
