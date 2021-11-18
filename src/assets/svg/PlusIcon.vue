19ae75c95bffe3e24a0c605b029a93455ab27707
behnam <salehinezhad.b@gmail.com>
2021-11-18 11:33:14 +0330

    update plus icon

--- src/assets/svg/PlusIcon.vue
+++ src/assets/svg/PlusIcon.vue
@@ -1,5 +1,5 @@
 <template>
-	<svg
+	<!-- <svg
 		aria-hidden="true"
 		data-prefix="fas"
 		data-icon="plus-circle"
@@ -9,6 +9,18 @@
 		<path
 			fill="currentColor"
 			d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"
+		/>
+	</svg> -->
+	<svg
+		aria-hidden="true"
+		data-prefix="fas"
+		data-icon="plus"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 448 512"
+	>
+		<path
+			fill="currentColor"
+			d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
 		/>
 	</svg>
 </template>
