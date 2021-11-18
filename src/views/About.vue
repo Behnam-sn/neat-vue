79798a74ffb32ffcd7855ab948847d32e7c2a53c
behnam <salehinezhad.b@gmail.com>
2021-11-18 08:29:35 +0330

    update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div>
+		<TitleBar title="About" />
+		<div class="px-7 lg:px-16">
+			<h1>This is an about page</h1>
+		</div>
+	</div>
 </template>
+
+<script>
+import TitleBar from "../components/TitleBar.vue";
+
+export default {
+	name: "About",
+	components: {
+		TitleBar,
+	},
+};
+</script>
