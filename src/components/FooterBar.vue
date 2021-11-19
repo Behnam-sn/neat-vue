7051dae8df0dbd777a554092f26507718d23e31a
behnam <salehinezhad.b@gmail.com>
2021-11-19 11:17:41 +0330

    add plus button

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,9 +1,3 @@
-<script>
-export default {
-	name: "FooterBar",
-};
-</script>
-
 <template>
 	<div
 		class="
@@ -11,13 +5,56 @@ export default {
 			fixed
 			bottom-0
 			w-full
-			shadow
+			h-12
+			shadow-2xl
 			bg-secondary
 			dark:bg-primary
 			transition
 			duration-500
 		"
 	>
-		footer
+		<div class="w-full h-full relative">
+			<div
+				id="btn"
+				class="
+					absolute
+					w-16
+					h-16
+					rounded-full
+					bg-primary
+					ring-8 ring-secondary
+					dark:ring-primary
+					flex
+					justify-center
+					items-center
+					cursor-pointer
+					transition
+					duration-500
+				"
+			>
+				<PlusIcon class="h-12 text-secondary transition duration-500" />
+			</div>
+		</div>
 	</div>
 </template>
+
+<script>
+import PlusIcon from "../assets/svg/PlusIcon.vue";
+
+export default {
+	name: "FooterBar",
+	components: {
+		PlusIcon,
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+#btn {
+	// left: 50%;
+	// right: 50%;
+	top: -65%;
+	left: 50%;
+	transform: translate(-50%, 0);
+}
+</style>
