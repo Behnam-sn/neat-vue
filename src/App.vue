6173046892864b64fb494b8a9b9f04f75b6f9eee
behnam <salehinezhad.b@gmail.com>
2021-11-19 21:32:57 +0330

    update footer bar

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,38 @@
 				</transition>
 			</router-view>
 		</div>
-		<FooterBar />
+		<FooterBar v-if="isNotNote">
+			<router-link
+				to="/addnote"
+				class="
+					footer-bar-btn
+					absolute
+					w-16
+					h-16
+					ring-8
+					rounded-full
+					bg-secondary
+					dark:bg-primary
+					ring-gray-300
+					dark:ring-gray-800
+					flex
+					justify-center
+					items-center
+					transition
+					duration-500
+				"
+			>
+				<PlusIcon
+					class="
+						h-12
+						text-primary
+						dark:text-secondary
+						transition
+						duration-500
+					"
+				/>
+			</router-link>
+		</FooterBar>
 	</div>
 </template>
 
@@ -20,11 +51,24 @@ import Navbar from "./components/Navbar.vue";
 import Menu from "./components/Menu.vue";
 import FooterBar from "./components/FooterBar.vue";
 
+import PlusIcon from "./assets/svg/PlusIcon.vue";
+
 export default {
 	name: "Home",
 	created: function () {
 		this.initTheme();
 		this.testToken();
+	},
+	computed: {
+		isNotNote() {
+			let path = this.$route.path;
+
+			if (path.includes("/addnote") || path.includes("/note")) {
+				return false;
+			} else {
+				return true;
+			}
+		},
 	},
 	methods: {
 		...mapMutations(["initTheme"]),
@@ -34,6 +78,7 @@ export default {
 		Navbar,
 		Menu,
 		FooterBar,
+		PlusIcon,
 	},
 };
 </script>
@@ -43,6 +88,12 @@ export default {
 	// font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
+}
+
+.footer-bar-btn {
+	top: -65%;
+	left: 50%;
+	transform: translate(-50%, 0);
 }
 
 .route-enter-active,
--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -6,55 +6,21 @@
 			bottom-0
 			w-full
 			h-12
-			shadow-2xl
-			bg-secondary
-			dark:bg-primary
+			shadow-xl
+			bg-gray-300
+			dark:bg-gray-800
 			transition
 			duration-500
 		"
 	>
 		<div class="w-full h-full relative">
-			<div
-				id="btn"
-				class="
-					absolute
-					w-16
-					h-16
-					rounded-full
-					bg-primary
-					ring-8 ring-secondary
-					dark:ring-primary
-					flex
-					justify-center
-					items-center
-					cursor-pointer
-					transition
-					duration-500
-				"
-			>
-				<PlusIcon class="h-12 text-secondary transition duration-500" />
-			</div>
+			<slot></slot>
 		</div>
 	</div>
 </template>
 
 <script>
-import PlusIcon from "../assets/svg/PlusIcon.vue";
-
 export default {
 	name: "FooterBar",
-	components: {
-		PlusIcon,
-	},
 };
 </script>
-
-<style lang="scss" scoped>
-#btn {
-	// left: 50%;
-	// right: 50%;
-	top: -65%;
-	left: 50%;
-	transform: translate(-50%, 0);
-}
-</style>
