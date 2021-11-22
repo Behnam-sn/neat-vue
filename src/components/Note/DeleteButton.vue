b68c915d12e03d74dc43739d52f35598531e848c
behnam <salehinezhad.b@gmail.com>
2021-11-23 00:36:11 +0330

    add delete confirm

--- src/components/Note/DeleteButton.vue
+++ src/components/Note/DeleteButton.vue
@@ -1,7 +1,7 @@
 <template>
-	<div>
+	<div class="relative">
 		<button
-			@click="deleteNote"
+			@click="toggleBox"
 			class="
 				w-14
 				h-14
@@ -21,6 +21,59 @@
 		>
 			<TrashIcon class="h-8" />
 		</button>
+		<transition name="bounce">
+			<div
+				v-if="confirmBox"
+				class="
+					absolute
+					right-2
+					bg-secondary
+					w-auto
+					min-w-max
+					py-5
+					px-10
+					m-2
+					rounded-md
+					shadow-md
+					z-10
+				"
+			>
+				<button
+					@click="deleteNote"
+					class="
+						font-Poppins-Bold
+						text-sm
+						bg-red-100
+						hover:bg-red-200
+						text-red-500
+						hover:text-red-600
+						p-4
+						m-1
+						rounded-md
+						transition
+						duration-500
+					"
+				>
+					Delete
+				</button>
+				<button
+					@click="toggleBox"
+					class="
+						font-Poppins-Bold
+						text-sm
+						hover:bg-gray-200
+						text-primary
+						p-4
+						m-1
+						rounded-md
+						transition
+						duration-500
+					"
+				>
+					Cancel
+				</button>
+			</div>
+		</transition>
 	</div>
 </template>
 
@@ -31,11 +84,36 @@ import TrashIcon from "../../assets/svg/TrashIcon.vue";
 
 export default {
 	name: "DeleteButton",
+	data: () => ({
+		confirmBox: false,
+	}),
 	methods: {
 		...mapActions(["deleteNote"]),
+		toggleBox() {
+			this.confirmBox = !this.confirmBox;
+		},
 	},
 	components: {
 		TrashIcon,
 	},
 };
 </script>
+
+<style lang="scss">
+.bounce-enter-active {
+	animation: bounce-in 0.5s cubic-bezier(0.4, 0, 0.2, 1);
+	transform-origin: top right;
+}
+.bounce-leave-active {
+	animation: bounce-in 0.5s cubic-bezier(0.4, 0, 0.2, 1) reverse;
+	transform-origin: top right;
+}
+@keyframes bounce-in {
+	0% {
+		transform: scale(0);
+	}
+	100% {
+		transform: scale(1);
+	}
+}
+</style>
