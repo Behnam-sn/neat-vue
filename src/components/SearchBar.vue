fd956609ba467940b0445cb9c814d2f5c591e44c
behnam <salehinezhad.b@gmail.com>
2021-11-24 12:13:45 +0330

    update searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,40 +1,33 @@
 <template>
-	<div
-		class="
-			flex
-			items-center
-			bg-gray-100
-			hover:bg-gray-200
-			dark:bg-gray-500 dark:hover:bg-gray-600
-			rounded-lg
-			px-6
-			my-7
-			transition
-			duration-500
-		"
-	>
-		<SeachIcon class="h-6 text-gray-300" />
+	<div class="flex items-center relative my-7">
+		<SeachIcon class="absolute left-4 h-6 text-gray-300" />
 		<input
 			v-model="search"
 			class="
 				w-full
-				bg-transparent
+				bg-gray-100
+				hover:bg-gray-200
+				focus:bg-gray-200
+				dark:bg-gray-500 dark:hover:bg-gray-400 dark:focus:bg-gray-400
+				rounded-lg
 				font-Rubik-Light
 				text-lg
-				px-3
+				px-12
 				py-3
 				focus:outline-none
+				transition
+				duration-500
 			"
 			type="text"
 			name="search"
 			placeholder="Search"
 		/>
-		<button v-show="search" @click="clearSerach">
+		<button v-show="search" @click="clearSerach" class="absolute right-4">
 			<CloseIcon
 				class="
 					h-6
 					text-gray-500
-					dark:text-gray-800
+					dark:text-gray-600
 					transition
 					duration-500
 				"
@@ -52,14 +45,14 @@ export default {
 	data: () => ({
 		search: undefined,
 	}),
-	components: {
-		SeachIcon,
-		CloseIcon,
-	},
 	methods: {
 		clearSerach() {
 			this.search = undefined;
 		},
+	},
+	components: {
+		SeachIcon,
+		CloseIcon,
 	},
 };
 </script>
