e47cfbe398bc53f217743cd1a21166f43cef7e6e
behnam <salehinezhad.b@gmail.com>
2021-12-01 10:15:59 +0330

    add author

--- src/components/ReadableNote.vue
+++ src/components/ReadableNote.vue
@@ -4,26 +4,40 @@
 			class="flex justify-between items-center mx-4 lg:mx-7 my-4 lg:my-6"
 		>
 			<GoBackButton />
+			<div
+				class="
+					mr-4
+					lg:mr-7
+					flex flex-col
+					lg:flex-row
+					items-end
+					justify-end
+				"
+			>
+				<DateAndTime
+					title="Created"
+					:value="note.created_at"
+					class="lg:mr-6"
+				/>
+				<DateAndTime title="Modified" :value="note.modified_at" />
+			</div>
 		</div>
 		<div
 			class="
-				mx-8
-				lg:mx-12
-				flex flex-col
-				lg:flex-row
-				items-end
+				flex
 				justify-end
+				font-Rubik-Medium
+				text-2xl
+				w-full
+				px-8
+				my-3
+				lg:px-20 lg:my-5
 			"
 		>
-			<DateAndTime
-				title="Created"
-				:value="note.created_at"
-				class="lg:mr-6"
-			/>
-			<DateAndTime title="Modified" :value="note.modified_at" />
+			Author: {{ note.author }}
 		</div>
 		<div
-			class="font-Rubik-Medium text-2xl w-full px-8 my-3 lg:px-20 lg:y-5"
+			class="font-Rubik-Medium text-2xl w-full px-8 lg:px-20 my-3 lg:my-5"
 			dir="auto"
 		>
 			{{ note.title }}
@@ -32,8 +46,9 @@
 			class="
 				w-full
 				px-8
+				lg:px-20
 				my-3
-				lg:px-20 lg:y-5
+				lg:my-5
 				font-Poppins-Light
 				text-xl
 				whitespace-pre-wrap
