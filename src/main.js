cb8f4c894846a63df547cc1999e96377953edda1
behnam <salehinezhad.b@gmail.com>
2022-01-02 11:28:10 +0330

    add .eslintignore

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
 
-// axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/"; // loacl backend
-axios.defaults.baseURL = "https://evening-plateau-38171.herokuapp.com/api/v1/"; // heroku backend
+axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/"; // loacl backend
+// axios.defaults.baseURL = "https://evening-plateau-38171.herokuapp.com/api/v1/"; // heroku backend
 
 axios.interceptors.response.use(undefined, function (error) {
 	if (error) {
